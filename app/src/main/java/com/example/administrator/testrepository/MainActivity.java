d2dacc67d83a1d1e72154fb095933e799fde0064
zhangjixiang2018 <44158103+zhangjixiang2018@users.noreply.github.com>
2018-11-16 14:58:17 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/testrepository/MainActivity.java
+++ app/src/main/java/com/example/administrator/testrepository/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("1.1");
+        System.out.println("1.2");
     }
 }
